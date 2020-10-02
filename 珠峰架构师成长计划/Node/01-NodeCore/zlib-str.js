528e01ccada09727cb9e667ad25a16c6df5617e9
zhoujin <1391020381@qq.com>
2020-10-02 12:23:24 +0800

    zlib-str

---
+++ 珠峰架构师成长计划/Node/01-NodeCore/zlib-str.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const zlib = require('zlib')
+const responseText = 'hello world'
+
+const server = http.createServer(function(req,res){
+    let acceptEncoding = req.headers['accept-encoding']
+    if(acceptEncoding.indexof('gzip')!=-1){
+        res.end(zlib.gzipSync(responseText))
+    }else{
+        res.end(responseText)
+    }
+})
+server.listen('3000',()=>{
+    console.log('server is listening 3000')
+})
